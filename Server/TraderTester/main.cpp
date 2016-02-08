af9762f27b6580a9a68bca353eafb57afed5280b
nupagadi <kuyanski@gmail.com>
2016-02-08 19:45:24 +0300

    inition tests ultimate

--- Server/TraderTester/main.cpp
+++ Server/TraderTester/main.cpp
@@ -18,7 +18,6 @@ BOOST_AUTO_TEST_CASE(hollow_inition_test1)
     BOOST_TEST(trader1.mOpenedOrders.empty());
 }
 
-
 BOOST_AUTO_TEST_CASE(hollow_inition_test2)
 {
     std::vector<std::string> ini_string;
@@ -61,17 +60,12 @@ BOOST_AUTO_TEST_CASE(inition_test2)
 {
     std::vector<std::string> ini_string;
     ini_string.push_back("123 10000000"); // id:123, balance:1k$
-    ini_string.push_back("1001 EURUSD 0 1 11234"); // order: id:1001, symbol, side, lot, price:1.1234
-    BOOST_REQUIRE_NO_THROW(Trader trader(ini_string));
-    Trader trader1(ini_string);
-    BOOST_TEST_REQUIRE(trader1.mOpenedOrders.size() == 1);
-    auto& order_ref = trader1.mOpenedOrders[0];
-    BOOST_TEST(order_ref.id == 1001);
-    BOOST_TEST(order_ref.symbol == "EURUSD");
-    BOOST_TEST(order_ref.lot == 1);
-    BOOST_TEST(order_ref.side == Trader::Order::Side::buy);
-    BOOST_TEST(order_ref.price == 11234);
-
+    ini_string.push_back("1001 EURUSD 0 1 11234 "); // order: id:1001, symbol, side, lot, price:1.1234
+    BOOST_CHECK_THROW(Trader trader(ini_string), Trader::InitionException);
+    ini_string[1] = "1001 EURUSD 0 1 11234 213";
+    BOOST_CHECK_THROW(Trader trader(ini_string), Trader::InitionException);
+    ini_string[1] = "1001 EURUSD 0 1";
+    BOOST_CHECK_THROW(Trader trader(ini_string), Trader::InitionException);
 }
 
 BOOST_AUTO_TEST_CASE(order_add_test)
@@ -101,53 +95,4 @@ BOOST_AUTO_TEST_CASE(order_add_test)
 
 }
 
-//BOOST_AUTO_TEST_CASE(creation_test)
-//{
-//    std::vector<std::string> vec{"123", "234", "345"};
-//    Trader t1("11", 10000000, vec);
-//    Trader t2("12", 9990000, std::move(vec));
-
-//    BOOST_TEST(t1.mId == "11");
-//    BOOST_TEST(t2.mId == "12");
-//    BOOST_TEST(t1.mBalance == 10000000);
-//    BOOST_TEST(t2.mBalance == 9990000);
-//    BOOST_TEST(t1.mOpenedOrders == t2.mOpenedOrders);
-//}
-
-//BOOST_AUTO_TEST_CASE(new_test)
-//{
-//    std::vector<std::string> vec{"123", "234", "345"};
-//    auto pt = new Trader("11", 10000, vec);
-//    BOOST_TEST(pt);
-//    BOOST_TEST(pt->mId == "11");
-//    BOOST_TEST(pt->mOpenedOrders.size() == 3);
-//    BOOST_TEST(pt->mOpenedOrders[1] == "234");
-//}
-
-//BOOST_AUTO_TEST_CASE(operations_test)
-//{
-//    std::vector<std::string> vec{"123", "234", "345"};
-//    Trader t1("11", 10000000, vec);
-
-//    t1 += "456";
-//    BOOST_TEST(t1.mOpenedOrders.size() == 4);
-//    BOOST_TEST(t1.mOpenedOrders[3] == "456");
-//    t1 += "456";
-//    BOOST_TEST(t1.mOpenedOrders.size() == 4);
-//    BOOST_TEST(t1.mOpenedOrders[3] == "456");
-
-//    t1 -= "123";
-//    BOOST_TEST(t1.mOpenedOrders.size() == 3);
-//    BOOST_TEST(t1.mOpenedOrders[1] == "345");
-//    t1 -= "123";
-//    BOOST_TEST(t1.mOpenedOrders.size() == 3);
-//    BOOST_TEST(t1.mOpenedOrders[1] == "345");
-
-//    t1 += "123";
-//    t1 += "567";
-//    t1 -= "456";
-//    BOOST_TEST(t1.mOpenedOrders.size() == 4);
-//    std::vector<std::string> vec2{"234", "345", "123", "567"};
-//    BOOST_TEST(t1.mOpenedOrders == vec2);
-//}
 
